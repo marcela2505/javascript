d402bb70c5cece805738a3029009fb2ebbc1f48a
Marcela Geovana <164965577+marcela2505@users.noreply.github.com>
2025-03-31 16:32:37 -0300

    .

---
+++ aula13/ambiente.js
@@ -0,0 +1,13 @@
+// Estrutura de repetição com teste lógico no INÍCIO
+var c = 1
+while (c <= 5) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+// Estrutura de repetição com teste lógico no FINAL
+var d = 1
+do {
+    console.log(`Passo ${d}`)
+    d++
+} while (d <= 5)
---
+++ aula14/ambiente.js
@@ -0,0 +1,5 @@
+// Estrutura de repetição com variável de controle
+for (var b = 1;b <= 5;b++) {
+    console.log(`Passo ${b}`)
+}
+console.log('FIM')
