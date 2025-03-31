a70feb3a741ea4dc2e3f1160368d3f9a017aa82d
Marcela Geovana <164965577+marcela2505@users.noreply.github.com>
2025-03-31 00:44:21 -0300

    .

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,19 @@
-alert('Olá!')
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var hora = new Date().getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        // BOM DIA
+        img.src = "imagens/fotomanha.jpg"
+        document.body.style.backgroundColor = "#f4d8a4"
+    } else if (hora >= 12 && hora < 18){
+        // BOA TARDE
+        img.src = "imagens/fototarde.jpg"
+        document.body.style.backgroundColor = "#b9846f"
+    } else {
+        // BOA NOITE
+        img.src = "imagens/fotonoite.jpg"
+        document.body.style.backgroundColor = "#100E1A"
+    }
+}
