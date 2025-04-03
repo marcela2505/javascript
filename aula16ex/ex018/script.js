105e583c1f62454574dfb9ab92d4c6e49d5df910
Marcela Geovana <164965577+marcela2505@users.noreply.github.com>
2025-04-03 16:41:40 -0300

    .

---
+++ aula16ex/ex018/script.js
@@ -0,0 +1,33 @@
+let numeros = []
+let soma = 0
+let res = document.getElementById('res')
+
+function adicionar() {
+    let txtnum = document.getElementById('txtnum')
+    let n = Number(txtnum.value)
+    let sellista = document.getElementById('sellista')
+    if (n > 100 || n < 1 || txtnum.value.length == 0 || numeros.includes(n)) {
+        alert('Valor inválido ou já encontrado na lista.')
+    } else {
+        numeros.push(n)
+        let item = document.createElement('option')
+        item.text = `Valor ${n} adicionado.`
+        item.value = `item${numeros.indexOf(n)}`
+        sellista.appendChild(item)
+        soma += n
+        res.innerHTML = ''
+    }
+}
+
+function finalizar() {
+    if (numeros.length == 0) {
+        alert('Adicione valores antes de finalizar')
+    } else {
+        res.innerHTML = `<p>Ao todo, temos ${numeros.length} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${Math.max(...numeros)}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${Math.min(...numeros)}.</p>`
+        res.innerHTML += `<p>Somando todos os valores temos ${soma}.</p>`
+        res.innerHTML += `<p>A média dos valores digitados é ${soma / numeros.length}.</p>`
+    }
+    
+}
